31516694f652a2f0fffebb8acf464819426109a7
dinesh16a <dineshtdb@.com>
2024-02-22 09:38:15 +0530

    sumatha

---
+++ Quiz app/index.js
@@ -0,0 +1,21 @@
+const qustions=[
+     {
+        qustion:"What is the largest animal in world",
+        answer:[
+            {text:"shark",correct:false},
+            {text:"blue whale",correct:true},
+            {text:"elephant",correct:false},
+            {text:"cat",correct:false},
+        ]
+     },
+     {
+        qustion:"What is the smallext animal in world",
+        answer:[
+            {text:"shark",correct:false},
+            {text:"blue whale",correct:false},
+            {text:"elephant",correct:false},
+            {text:"cat",correct:true},
+        ]
+     },
+     
+];
