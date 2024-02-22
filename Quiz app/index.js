53d0d3a3cbb3c54243e74b153a141cbed5446127
dinesh16a <dineshtdb@.com>
2024-02-22 10:34:46 +0530

    final push

--- Quiz app/index.js
+++ Quiz app/index.js
@@ -58,9 +58,41 @@ function resetState(){
 function selectAnswer(e){
     const selectBtn=e.target;
     const iscorrect=selectBtn.dataset.correct==="true";
-    if(iscorrect)
+    if(iscorrect){
     selectBtn.classList.add("correct");
+    score++;
+    }
    else
    selectBtn.classList.add("incorrect");
+ Array.from(answerBtn.children).forEach(button=>{
+    if(button.dataset.correct==="true"){
+        button.classList.add("correct");
+    }
+    button.disabled=true;
+
+});
+nextButton.style.display="block";
 }
+function handleNextButton(){
+    i++;
+    if(i<qustions.length){
+        showQuestion();
+    }else{
+        showscore();
+    }
+    
+}
+function showscore(){
+    resetState();
+    qustionElement.innerHTML=`you scored ${score}`;
+    nextButton.innerHtml="play again";
+    nextButton.style.display="block";
+}
+nextButton.addEventListener("click",()=>{
+    if(i<qustions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+})
 startQuiz();
